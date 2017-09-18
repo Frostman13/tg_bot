916484c14fda70fdfe4e4ae5d24bf7e62c76891a
Anton Morozov <frostman13@mail.ru>
2017-09-18 23:17:04 +0300

    new key_calc version

--- calc.py
+++ calc.py
@@ -1,9 +1,72 @@
 import platform
 import codecs
+import re
 # Калькуляторы
-CALC_SYMBOLS = ['0','1','2','3','4','5','6','7','8','9','+','-','*','÷','=']
-DIGITS = ['0','1','2','3','4','5','6','7','8','9']
+CALC_SYMBOLS = ['0','1','2','3','4','5','6','7','8','9','+','-','*','÷','=','.']
+WORD_DIGITS_DICT = {'нуль':'0','ноль':'0','один':'1','два':'2','три':'3',
+'четыре':'4','пять':'5','шесть':'6','семь':'7','восемь':'8','девять':'9'}
+WORD_CALC_DICT = {'плюс':'+','минус':'-','умножить':'*'}
+WORD_CALC_DICT_ALTERNATIVE = {'умножить на':'*','поделить на':'÷','разделить на':'÷'}
+DELETE_WORDS = {'сколько будет':'','посчитай':''}
 
+calc_data_dict = {}
+
+def key_calc(text, chat_id):
+    if text == "=":
+        result = chat_calc(calc_data_dict.get(chat_id) + '=')        
+        calc_data_dict[chat_id] = None
+    else:
+        if calc_data_dict.get(chat_id) == None:
+            calc_data_dict[chat_id] = text
+        else:
+            calc_data_dict[chat_id] += text
+        result = 'calc_continue'
+    return result
+
+def clear_calc(chat_id):
+    if calc_data_dict.get(chat_id) != None:
+        calc_data_dict[chat_id] = None
+
+def chat_calc(str):
+    numbers = re.findall(r'\d+\.?\d?\d?\d?\d?',str)
+    if len(numbers) < 2:
+        result = 'Не хватает аргументов для расчета'
+    else:
+        for symbol in str:
+            if symbol not in CALC_SYMBOLS:
+                str = str.replace(symbol, '')
+        if str.find('+') != -1:
+            result = float(numbers[0]) + float(numbers[1])
+        elif str.find('-') != -1:
+            result = float(numbers[0]) - float(numbers[1])
+        elif str.find('*') != -1:
+            result = float(numbers[0]) * float(numbers[1])
+        elif str.find('÷') != -1:
+            if float(numbers[1]) == 0:
+                result = 'Ошибка деления на ноль'
+            else:
+                result = float(numbers[0]) / float(numbers[1])
+                result = round(result,4)
+        result = 'Результат: {}{}'.format(str,result)
+    return result
+
+def word_calc(str):
+    str = str.lower()
+    print(str)
+    for step in DELETE_WORDS:
+        str = re.sub(step,DELETE_WORDS[step],str)
+    for step in WORD_DIGITS_DICT:
+        str = re.sub(step,WORD_DIGITS_DICT[step],str)
+    for step in WORD_CALC_DICT_ALTERNATIVE:
+        str = re.sub(step,WORD_CALC_DICT_ALTERNATIVE[step],str)
+    for step in WORD_CALC_DICT:
+        str = re.sub(step,WORD_CALC_DICT[step],str)
+    for symbol in str:
+        if symbol not in CALC_SYMBOLS:
+            str = str.replace(symbol, '')
+    return chat_calc(str + '=')
+
+# возвращает корректный слэш, в зависимости от ОС
 def correct_slash():
     if platform.system() == "Windows":
         result = '\\'
@@ -11,33 +74,8 @@ def correct_slash():
         result = '/'
     return result
 
-def chat_calc(str):
-    numbers = ['','']
-    numbers_count = 0
-    for symbol in str:
-        if symbol not in CALC_SYMBOLS:
-            str = str.replace(symbol, '')
-    for symbol in str:
-        if symbol in DIGITS:
-            numbers[numbers_count] += symbol
-        else:
-            numbers_count +=1
-    if str.find('+') != -1:
-        result = int(numbers[0]) + int(numbers[1])
-    elif str.find('-') != -1:
-        result = int(numbers[0]) - int(numbers[1])
-    elif str.find('*') != -1:
-        result = int(numbers[0]) * int(numbers[1])
-    elif str.find('÷') != -1:
-        if int(numbers[1]) == 0:
-            result = 'Ошибка деления на ноль'
-        else:
-            result = float(numbers[0]) / float(numbers[1])
-            result = round(result,2)
-    return('Результат вычислений: {}{}'.format(str,result))
-
-def key_calc(text, chat_id):
-    print(text)
+# не используется
+def key_calc_txt_file(text, chat_id):
     calc_file_name = 'calc{}calc{}.txt'.format(correct_slash(), chat_id)
     if text == "=":
         with open(calc_file_name, 'r', encoding = 'utf-8') as local_file:
@@ -49,14 +87,31 @@ def key_calc(text, chat_id):
             result = 'calc_continue'
     return result
 
-def clear_calc(chat_id):
+# не используется
+def clear_calc_txt_file(chat_id):
     calc_file_name = 'calc{}calc{}.txt'.format(correct_slash(), chat_id)
     with open(calc_file_name, 'w') as local_file:
         local_file.write('')
 
 
 if __name__ == '__main__':
-    print(chat_calc('6÷3='))
-    chat_calc('25  55 - 33  31  =')
+    # str = 'девять      умножить на  шесть'
+    # print(str)
+    # print(word_calc(str))
+    # print(chat_calc(word_calc(str)))
+    # print(WORD_DIGITS_DICT['один'])
+    # print(WORD_CALC_DICT_ALTERNATIVE['разделить на'])
+    # print(chat_calc('6÷3='))
+    # chat_calc('25  55 - 33  31  =')
+
     #calc('6/0=')
     # calc('25  55 + 33  31  =')
+    keyp_calc('1',192204203)
+    # keyp_calc('1',1)
+    keyp_calc('=',192204203)
+    print(calc_data_dict)
+    str1 = "ававлалзвла 1115.155552 ÷ ыывы 22.2 5 sdsds ="
+    # res = re.findall(r'\d*\.\d*',str1) + re.findall(r'[0-9]+',str1)
+    res = re.findall(r'\d+\.?\d?\d?\d?\d?',str1)
+    print (res)
+    # print(chat_calc(str1))
--- tg_bot.py
+++ tg_bot.py
@@ -1,5 +1,6 @@
 # Импортируем нужные компоненты
 import tg_bot_settings, astrology, word_handler, calc
+import re
 import telegram
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 from datetime import datetime, date, time
@@ -9,10 +10,9 @@ from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, Callb
 import logging
 logging.basicConfig(format='%(name)s - %(asctime)s - %(levelname)s - %(message)s',
                     level=logging.INFO,
-                    filename='tg_bot.log'
-                    )
+                    filename='tg_bot.log')
 
-bot = telegram.Bot(tg_bot_settings.TELEGRAM_API_KEY)
+
 
 # Приветствие
 def start_bot(bot, update):
@@ -39,80 +39,56 @@ def wordcount_bot(bot, update, args):
         update.message.reply_text(word_handler.word_count(message_text))
 
 def chat_bot(bot, update):
+    # print(update)
     chat_id = update.message.chat.id
     text = update.message.text
+    # print(text)
+
     if text in calc.CALC_SYMBOLS:
-        response = calc.key_calc(text,update.message.chat.id)
+        response = calc.key_calc(text, chat_id)
         if response != 'calc_continue':
             update.message.reply_text(response)
-            clear_keyboards(bot,chat_id)
     elif text.strip()[-1] == "=":
         update.message.reply_text(calc.chat_calc(text))
     elif text == 'Esc':
         clear_keyboards(bot,chat_id)
+    elif (text[:13] == 'сколько будет') or (text[:9] == 'посчитай'):
+        update.message.reply_text(calc.word_calc(text))
+    elif re.search(r'ближайшее полнолуние',text):
+        update.message.reply_text('Ближайшее полнолуние: {}'.format(astrology.next_full_moon(datetime.now())))
     else:
         update.message.reply_text(text)
     logging.info(text)
 
 def calc_bot(bot, update, args):
-    calc.clear_calc(update.message.chat.id)
+    chat_id = update.message.chat.id
+    calc.clear_calc(chat_id)
     custom_keyboard = [['1', '2','3','÷'], ['4', '5','6','*'],['7', '8','9','-'],['Esc', '0','+','=']]
     reply_markup = telegram.ReplyKeyboardMarkup(custom_keyboard)
-    bot.send_message(chat_id = update.message.chat.id, 
-                    text = "Используйте клавиатуру", 
-                    reply_markup = reply_markup)
+    bot.send_message(chat_id=chat_id, 
+                    text="Используйте клавиатуру", 
+                    reply_markup=reply_markup)
 
 def clear_keyboards(bot, chat_id):
     reply_markup = telegram.ReplyKeyboardRemove(remove_keyboard=True)
-    bot.send_message(chat_id = chat_id,
-                    text = "Наберите новую команду: /start /planet /calc /wordcount",
-                    reply_markup = reply_markup)
+    bot.send_message(chat_id=chat_id,
+                    text="Наберите новую команду: /start /planet /calc /wordcount",
+                    reply_markup=reply_markup)
 
 
 
 def main():
-    updater = Updater(tg_bot_settings.TELEGRAM_API_KEY)
-    
+    updater = Updater(tg_bot_settings.TELEGRAM_API_KEY)    
     dp = updater.dispatcher
+
     dp.add_handler(MessageHandler(Filters.text, chat_bot))
     dp.add_handler(CommandHandler("start", start_bot))
     dp.add_handler(CommandHandler("planet", planet_bot, pass_args=True))
     dp.add_handler(CommandHandler("calc", calc_bot, pass_args=True))
     dp.add_handler(CommandHandler("wordcount", wordcount_bot, pass_args=True))
 
-    # На будущее
-    # dp.add_handler(CommandHandler("button", button_bot))
-    # dp.add_handler(CallbackQueryHandler(button))
-
     updater.start_polling()
     updater.idle()
-
-    chat_id = update.message.chat.id
-
-# bot.sendMessage(chat_id,'Бот запущен. Команды: /start /planet /button',disable_notification=True)
-
-# На будущее
-# def button(bot, update):
-#     query = update.callback_query
-
-#     bot.edit_message_text(text="Selected option: %s" % query.data,
-#                           chat_id=query.message.chat_id,
-#                           message_id=query.message.message_id)
-
-# def button_bot(bot,update):
-#     keyboard = [[InlineKeyboardButton("Option 1", callback_data='1'),
-#                  InlineKeyboardButton("Option 2", callback_data='2'),
-#                  InlineKeyboardButton("Option 3", callback_data='3'),
-#                  InlineKeyboardButton("Option 4", callback_data='4')],
-
-#                 [InlineKeyboardButton("Option 5", callback_data='5'),
-#                 InlineKeyboardButton("Option 6", callback_data='6')]]
-
-#     reply_markup = InlineKeyboardMarkup(keyboard)
-
-#     update.message.reply_text('Please choose:', reply_markup=reply_markup)
-#     update.message.reply_text(keyboard)
-
 
 if __name__ == '__main__':  
     logging.info('Bot started')
